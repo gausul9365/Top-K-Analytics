86f9fde921bd8a25f3d79d8d66e52da83362829b
gausul9365 <gausul.analytics@gmail.com>
2026-01-10 19:35:34 +0530

    TopKResponse module added

--- src/main/java/com/finding/top_k/dto/TopKResponse.java
+++ src/main/java/com/finding/top_k/dto/TopKResponse.java
@@ -1,5 +1,20 @@
 package com.finding.top_k.dto;
 
-public class TopKResponse
-{
+public class TopKResponse {
+
+    private String itemId;
+    private long count;
+
+    public TopKResponse(String itemId, long count) {
+        this.itemId = itemId;
+        this.count = count;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public long getCount() {
+        return count;
+    }
 }
